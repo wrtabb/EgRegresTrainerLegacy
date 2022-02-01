e5434c9d2b2600779d0beae815ffff01cc376cb7
Robert Tabb <wrtabb@gmail.com>
2022-02-01 12:09:34 -0600

    updated variables names in scripts/runSCRegTrainings.py

--- scripts/runSCRegTrainings.py
+++ scripts/runSCRegTrainings.py
@@ -12,8 +12,8 @@ def main():
 
     parser = argparse.ArgumentParser(description='runs the SC regression trainings')
     parser.add_argument('--era',required=True,help='year to produce for, 2016, 2017, 2018 are the options')
-    parser.add_argument('--input_dir','-i',default='/mercury/data1/harper/EgRegsNtups',help='input directory with the ntuples')
-    parser.add_argument('--output_dir','-o',default="results",help='output dir')
+    parser.add_argument('--input_dir','-i',default='/home/hep/wrtabb/Egamma/input_trees/HLTRun3',help='input directory with the ntuples')
+    parser.add_argument('--output_dir','-o',default="/home/hep/wrtabb/Egamma/EgRegresTrainerLegacy/regressions/HLTRun3Regression",help='output dir')
     args = parser.parse_args()
 
     #step 1, run calo only regression on the ideal IC to get the mean
@@ -25,31 +25,18 @@ def main():
 
     #setup the selection (event number cuts come later)
     cuts_name = "stdCuts" 
-    base_ele_cuts = "(mc.energy>0 && ssFrac.sigmaIEtaIEta>0 && ssFrac.sigmaIPhiIPhi>0 && {extra_cuts})"
+    base_ele_cuts = "(eg_energy>0 && eg_sigmaIEtaIEta>0 && eg_sigmaIPhiIPhi>0 && {extra_cuts})"
     
     #prefixes all the regressions produced
-    if args.era=='2016':
-        base_reg_name = "scReg2016UL"
-        input_ideal_ic  = "{}/DoubleElectron_FlatPt-1To300_2016ConditionsFlatPU0to70ECALGT_105X_mcRun2_asymptotic_IdealEcalIC_newSR_v2-v2_AODSIM_EgRegTreeV5.root".format(args.input_dir)
-        input_real_ic = "{}/DoubleElectron_FlatPt-1To300_2016ConditionsFlatPU0to70RAW_105X_mcRun2_asymptotic_newECALSR_v2-v2_AODSIM_EgRegTreeV5.root".format(args.input_dir)
-        ideal_eventnr_cut = "evt.eventnr%5==0"  #4million electrons
-        real_eventnr_cut = "evt.eventnr%5==1" #4million electrons
+    if args.era=='2021':
+        base_reg_name = "HLTReg2021"
+        input_ideal_ic  = "{}/.root".format(args.input_dir)
+        input_real_ic = "{}/.root".format(args.input_dir)
+        ideal_eventnr_cut = "eventnr%5==0"  #4million electrons
+        real_eventnr_cut = "eventnr%5==1" #4million electrons
 
-    elif args.era=='2017':
-        base_reg_name = "scReg2017UL"    
-        input_ideal_ic  = "{}/DoubleElectron_FlatPt-1To300_2017ConditionsFlatPU0to70ECALGT_105X_mc2017_realistic_IdealEcalIC_v5-v2_AODSIM_EgRegTreeV1_extraVars.root".format(args.input_dir)
-        input_real_ic = "{}/DoubleElectron_FlatPt-1To300_2017ConditionsFlatPU0to70_105X_mc2017_realistic_v5-v2_AODSIM_EgRegTreeV1_4.root".format(args.input_dir)   
-        ideal_eventnr_cut = "evt.eventnr%2==0"
-        real_eventnr_cut = "evt.eventnr%2==0" #events in the ntuple are different so can get away with this
-
-    elif args.era=='2018':
-        base_reg_name = "scReg2018UL"    
-        input_ideal_ic  = "{}/DoubleElectron_FlatPt-1To300_2018ConditionsFlatPU0to70ECALGT_105X_upgrade2018_realistic_IdealEcalIC_v4-v1_AODSIM_EgRegTreeV5_partStatsV2.root".format(args.input_dir)
-        input_real_ic = "{}/DoubleElectron_FlatPt-1To300_2018ConditionsFlatPU0to70RAW_105X_upgrade2018_realistic_v4-v1_AODSIM_EgRegTreeV5_partStatsV2.root".format(args.input_dir)    
-        ideal_eventnr_cut = "evt.eventnr%5==0"  #4million electrons
-        real_eventnr_cut = "evt.eventnr%5==1" #4million electrons
     else:
-        raise ValueError("era {} is invalid, options are 2016/2017/2018".format(era))
+        raise ValueError("era {} is invalid, only option is 2021".format(era))
 
     
     regArgs = RegArgs()
@@ -57,7 +44,7 @@ def main():
     regArgs.input_testing = str(input_ideal_ic)
     regArgs.set_sc_default()
     regArgs.cfg_dir = "configs"
-    regArgs.out_dir = args.output_dir
+    regArgs.out_dir = "{}/resultsSC".format(args.output_dir)
     regArgs.cuts_name = cuts_name
     regArgs.base_name = "{}_IdealIC_IdealTraining".format(base_reg_name)
     regArgs.cuts_base = base_ele_cuts.format(extra_cuts = ideal_eventnr_cut)
@@ -95,7 +82,7 @@ steps to be run:
     regArgs.base_name = "{}_RealIC_RealTraining".format(base_reg_name)
     regArgs.input_training = input_for_res_training
     regArgs.input_testing = input_for_res_training
-    regArgs.target = "mc.energy/(sc.rawEnergy*regIdealMean)"
+    regArgs.target = "eg_gen_energy/(eg_rawEnergy*regIdealMean)"
     regArgs.fix_mean = True
     regArgs.cuts_base = base_ele_cuts.format(extra_cuts = real_eventnr_cut)
     if run_step3: regArgs.run_eb_and_ee()
